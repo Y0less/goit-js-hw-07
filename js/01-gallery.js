c1202a0f7e71aaf6ed094b371d7d09b9fb5b84eb
Oleksandr Stakhurskyi <yoles0@gmail.com>
2023-06-21 00:48:13 +0300

    partial completion

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,69 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryGrid = document.querySelector(".gallery");
+// console.log(galleryItems); //replace!
 
-console.log(galleryItems);
+importMarkupInGallery(generateMarkup(galleryItems));
+
+galleryGrid.addEventListener("click", handleClickOnImgPreview);
+
+function generateMarkup(galleryObj) {
+  return galleryObj
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+    )
+    .join("");
+}
+
+function importMarkupInGallery(markup) {
+  galleryGrid.innerHTML = markup;
+}
+
+function handleClickOnImgPreview(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const previewSrc = evt.target.src;
+  const imageObj = findImageObj(previewSrc, galleryItems);
+  openModalWithImageObj(imageObj);
+}
+
+/**
+ * in array of objects finding a corresponding object by image preview
+ * @param {string} previewImage - preview image
+ * @param {array} galleryArr - array of objects
+ * @returns {object}
+ */
+function findImageObj(previewImage, galleryArr = {}) {
+  return galleryArr.find(({ preview }) => preview === previewImage);
+}
+
+/**
+ * generates and opens basic modal window with image
+ * requires "basicLightbox" library
+ * receives object
+ * @param {object.key: string} original - image URL
+ * @param {object.key: string} description - description of the image
+ */
+function openModalWithImageObj({ original, description }) {
+  const modal = basicLightbox.create(`
+	<img
+            class="modal-image"
+            src="${original}"
+            alt="${description}"
+            width="800" height="600"
+          />
+`);
+  modal.show();
+}
